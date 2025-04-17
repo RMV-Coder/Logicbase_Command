6165a9434aac858727b3d23c67bc2b811049a8d5
RMV-Coder <70832774+RMV-Coder@users.noreply.github.com>
2025-04-18 01:03:47 +0800

    debugging validate

--- logicbase_command/src/app/api/validate/route.ts
+++ logicbase_command/src/app/api/validate/route.ts
@@ -3,11 +3,14 @@ import { jwtVerify } from 'jose'
 import { NextResponse, NextRequest } from 'next/server'
 export async function POST(req: NextRequest) {
     const { token } = await req.json()
+    console.log('Token:', token)
     const secret = process.env.JWT_SECRET as unknown as Uint8Array;
+    console.log('Secret:', secret)
     try {
-      const { payload } = await jwtVerify(token, secret)
-      return NextResponse.json({ valid: true, role: payload.role })
+        const { payload } = await jwtVerify(token, secret)
+        console.log('Payload:', payload)
+        return NextResponse.json({ valid: true, role: payload.role })
     } catch {
-      return NextResponse.json({ valid: false }, { status: 401 })
+        return NextResponse.json({ valid: false }, { status: 401 })
     }
   }
