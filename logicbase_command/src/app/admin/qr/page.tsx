4e327dfd7caf030178c6abfbada0b561edd0873a
RMV-Coder <70832774+RMV-Coder@users.noreply.github.com>
2025-04-18 00:55:03 +0800

    Improved UI of admin qr page

--- logicbase_command/src/app/admin/qr/page.tsx
+++ logicbase_command/src/app/admin/qr/page.tsx
@@ -1,9 +1,10 @@
 // app/admin/qr/page.tsx
 'use client'
 import { useState } from 'react'
-import { Button, Form, Modal, Input } from 'antd'
+import { Button, Form, Modal, Input, Layout, Space } from 'antd'
 import { useQRCode } from 'next-qrcode';
 import { DateTime } from "luxon";
+const { Header, Content, Footer } = Layout;
 interface FormValues {
     email: string
 }
@@ -48,36 +49,52 @@ export default function QRGenerator() {
 
   return (
     <>
-      <Button onClick={generateQR}>Generate Employee QR</Button>
-      <Button onClick={()=>setIsEmail(true)}>Send Registration Link via Email</Button>
-      <Modal open={isEmail} onCancel={() => setIsEmail(false)} title="Send Registration Link via Email">
-        <Form onFinish={sendEmail}
-        form={form}
-        // size='large'
-        name='emailForm'>
-            <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please input employee's email" }]}>
-                <Input type="email" placeholder="Enter employee's email address" />
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" loading={loading}>Send Email</Button>
-            </Form.Item>
-        </Form>
-      </Modal>
-      <Modal open={qr!==''&& !isEmail} onCancel={() => setQr('')}>
-        {/* <div dangerouslySetInnerHTML={{ __html: qr || '' }} /> */}
-        <p>Scan this QR to register new employees (expires in {DateTime.now().plus({ hours: 1 }).toFormat('DD hh:mm a')})</p>
-        <SVG
-            text={qr}
-            options={{
-                margin: 2,
-                width: 200,
-                color: {
-                dark: '#010599FF',
-                light: '#FFBF60FF',
-                },
-            }}
-            />
-      </Modal>
+    <Layout style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
+        <Header className="bg-white shadow-sm">
+            <h1 className="text-2xl font-bold">Logicbase Command | Admin</h1>
+        </Header>
+        <Content className="p-4" style={{flex:1, gap: '1rem', display: 'flex', flexDirection: 'column'}}>
+            <div className="bg-white p-4 rounded shadow-sm">
+                <h2 className="text-xl font-semibold mb-4">Generate QR Code for Employee Registration</h2>
+                <p>Click the button below to generate a QR code for employee registration.</p>
+            </div>
+            <Space>
+                <Button onClick={generateQR}>Generate Employee QR</Button>
+                <Button onClick={()=>setIsEmail(true)}>Send Registration Link via Email</Button>
+            
+            </Space>
+            <Modal open={isEmail} onCancel={() => setIsEmail(false)} title="Send Registration Link via Email">
+                <Form onFinish={sendEmail}
+                form={form}
+                // size='large'
+                name='emailForm'>
+                    <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please input employee's email" }]}>
+                        <Input type="email" placeholder="Enter employee's email address" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" loading={loading}>Send Email</Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
+            <Modal open={qr!==''&& !isEmail} onCancel={() => setQr('')}>
+                {/* <div dangerouslySetInnerHTML={{ __html: qr || '' }} /> */}
+                <p>Scan this QR to register new employees (expires in {DateTime.now().plus({ hours: 1 }).toFormat('DD hh:mm a')})</p>
+                <SVG
+                    text={qr}
+                    options={{
+                        margin: 2,
+                        width: 200,
+                        color: {
+                        dark: '#010599FF',
+                        light: '#FFBF60FF',
+                        },
+                    }}
+                    />
+            </Modal>
+        </Content>
+        <Footer className="text-center">Logicbase Command ©{new Date().getFullYear()} Developed by Raymond Valdepeñas</Footer>
+    </Layout>
+      
     </>
   )
 }
